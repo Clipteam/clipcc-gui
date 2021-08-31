386688d2175d34db777350e90551bbf333042467
SinanGentoo <sinangentoo@gmail.com>
2021-08-31 09:58:31 +0800

    只差width和height

--- src/components/contributor-modal/contributor-list.js
+++ src/components/contributor-modal/contributor-list.js
@@ -1,32 +1,4 @@
-/*
-export const contributorList = '(In no particular order)\n' +
-'Alex Cui - G-Editor developer / ClipCC lead developer, ClipCommunity developer\n' +
-'SinanGentoo - G-Editor / ClipCC lead developer，ClipCommunity developer，Clipteam founder\n' +
-'solstice23 - G-Editor lead developer\n' +
-'e4361 - G-Editor developer\n' +
-'SparrowHe - G-Editor / ClipCC developer，advocacy support，mascot\n' +
-'SteveXMH - ClipCC developer\n' +
-'Zerlight - ClipCC artistic support\n' +
-'Hydrostic - ClipCC developer， ClipCommunity lead developer\n' +
-'Frank_782 ClipCC developer\n' +
-'BleShi - Clipteam\'s domain/server sponsor\n' +
-'Jason - ClipCC developer，advocacy support\n' +
-'SoilZhu - ClipCC artistic support, chuunibyo mode contributor.\n' +
-'waterblock79 - ClipCC extension store developer\n' +
-'Yuan3old - ClipCC advocacy support\n' +
-'Someone_Yang - ClipCC advocacy support\n' +
-'LMlanmei - ClipCC Traditional Chinese i18n contributor\n' +
-'xiaoji4093 - ClipCC sponsor\n' +
-'JasonXu - ClipCC sponsor（200 CNY）\n' +
-'Unknown contributor - ClipCC sponsor（60 CNY）\n' +
-'LyricEpic - ClipCC sponsor（19.98 CNY）\n' +
-'Cya_Rice - ClipCC sponsor（11.4 CNY）\n' +
-'Afadian_c5qE - ClipCC sponsor （30 CNY）\n' +
-'mbzzw - ClipCommunity sponsor（60 CNY）\n' +
-'All SPONSORS who helped the clipteam through the difficult times in August 2021\n' +
-'\nAnd you.';
-*/
-
+/* eslint-disable linebreak-style */
 export const contributorList = {
     tips: '(In no particular order)',
     alexcui: 'Alex Cui - G-Editor developer / ClipCC lead developer, ClipCommunity developer',
--- src/components/contributor-modal/contributor-modal.jsx
+++ src/components/contributor-modal/contributor-modal.jsx
@@ -26,7 +26,32 @@ const ContributorModal = ({
     >
         <Box className={styles.body}>
             <p>
-                <strong>{contributorList}</strong>
+                <strong>{contributorList.tips}</strong>
+                <p>{contributorList.sinangentoo}<br /></p>
+                <p>{contributorList.alexcui}<br /></p>
+                <p>{contributorList.solstice23}<br /></p>
+                <p>{contributorList.bleshi}<br /></p>
+                <p>{contributorList.e4361}<br /></p>
+                <p>{contributorList.sparrowhe}<br /></p>
+                <p>{contributorList.jasonjia}<br /></p>
+                <p>{contributorList.xiaoji4093}<br /></p>
+                <p>{contributorList.hydrostic}<br /></p>
+                <p>{contributorList.zerlight}<br /></p>
+                <p>{contributorList.frank782}<br /></p>
+                <p>{contributorList.stevexmh}<br /></p>
+                <p>{contributorList.jasonxu}<br /></p>
+                <p>{contributorList.lmlanmei}<br /></p>
+                <p>{contributorList.waterblock79}<br /></p>
+                <p>{contributorList.someoneyoung}<br /></p>
+                <p>{contributorList.yuan3old}<br /></p>
+                <p>{contributorList.soilzhu}<br /></p>
+                <p>{contributorList.cyarice}<br /></p>
+                <p>{contributorList.lyricepic}<br /></p>
+                <p>{contributorList.unknown}<br /></p>
+                <p>{contributorList.mbzzw}<br /></p>
+                <p>{contributorList.afadian}<br /></p>
+                <p>{contributorList.august}<br /></p>
+                <p>{contributorList.you}<br /></p>
             </p>
         </Box>
     </Modal>
