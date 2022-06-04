c82d39fe67ba0d62bf984f3313544020005868cb
frank <frank_782@qq.com>
2022-06-04 08:43:59 +0800

    将Sentry更改为内嵌 同时增加rrweb以更方便重现错误

--- src/playground/index.jsx
+++ src/playground/index.jsx
@@ -5,10 +5,25 @@ import analytics from '../lib/analytics';
 import AppStateHOC from '../lib/app-state-hoc.jsx';
 import BrowserModalComponent from '../components/browser-modal/browser-modal.jsx';
 import supportedBrowser from '../lib/supported-browser';
+import * as Sentry from '@sentry/react';
+import SentryRRWeb from '@sentry/rrweb';
+import {isProd, appVersion, appVersionFull} from '../lib/app-info';
 import styles from './index.css';
 
 // Register "base" page view
 analytics.pageview('/');
+Sentry.init({
+    dsn: 'https://ed881d9f133e457d8cdca25202200c3f@o1098997.ingest.sentry.io/6123390',
+    integrations: [
+        new SentryRRWeb({
+            checkoutEveryNms: 2 * 60 * 1000
+        })
+    ],
+    release: isProd ? appVersion : appVersionFull,
+    environment: isProd ? 'stable' : 'canary',
+    tracesSampleRate: 0.5
+});
+global.Sentry = Sentry;
 
 const appTarget = document.createElement('div');
 appTarget.className = styles.app;
